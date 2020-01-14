e4d7d2e1fcf69f0702eb1d8a268eef5b6baed42c
Pepui <59765290+Pepui@users.noreply.github.com>
2020-01-14 20:37:34 +0100

    Initial commit

---
+++ weather-app/src/app_component/weather.component.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const weather = () => {
+    return (
+
+        <div className="container text-light">
+           <div className="cards pt-4">
+    <h1>{props.city}</h1>
+           <h5 className="py-4">
+            <i className={"wi ${props.weathericon} display-1"}/>
+            </h5>                          
+            {props.temp_celsius ? (
+<h1>className=<py-2>
+</py-2>props.temp_celsius)&deg;</h1>
+            ):null}
+           </div>
+        {/** show max and min temp*/}
+        {minmaxTemp(propos.temp_min, props.temp.max)}
+        <h4 className="py-3">{props.description}</h4>
+        
+        </div>
+        
+    );
+}
+
+function minmaxTemp(min,max){
+    if(min&&max){
+        return(
+            <h3>
+    <span className="px-4">{min}&deg;</span>
+    <span className="px-4">{max}&deg;</span>
+            </h3>
+        )
+    }
+    
+
+}
+
+export default weather;
